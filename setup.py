986d17b6f98b810e66e5f84d9606318a14f28d9b
James Ravenscroft <ravenscroftj@gmail.com>
2013-06-13 08:34:02 +0100

    Started writing setup script

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distribute import setup, find_packages
+
+setup(
+    name = "SAPIENTA",
+    version = 0.1,
+    packages = find_packages(),
+
+    #install requirements
+    install_requires = ['text-sentence>=0.14'],
+
+    author="James Ravenscroft",
+    author_email = "ravenscroft@papro.org.uk",
+    description = "Toolkit for annotating XML scientific papers with CoreSC labels",
+    url="http://www.sapientaproject.com/"
+        
+)
