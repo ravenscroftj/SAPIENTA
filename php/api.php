4114188c3c273622decfaeb2fb363805b168336c
James Ravenscroft <ravenscroftj@gmail.com>
2013-08-01 11:26:21 +0100

    Fixed API json problems

--- php/api.php
+++ php/api.php
@@ -9,6 +9,77 @@ require("sapienta.php");
 
 
 /**
+ * Converts an associative array of arbitrary depth and dimension into JSON representation.
+ *
+ * NOTE: If you pass in a mixed associative and vector array, it will prefix each numerical
+ * key with "key_". For example array("foo", "bar" => "baz") will be translated into
+ * {'key_0': 'foo', 'bar': 'baz'} but array("foo", "bar") would be translated into [ 'foo', 'bar' ].
+ *
+ * @param $array The array to convert.
+ * @return mixed The resulting JSON string, or false if the argument was not an array.
+ * @author Andy Rusterholz
+ */
+function array_to_json( $array ){
+
+    if( !is_array( $array ) ){
+        return false;
+    }
+
+    $associative = count( array_diff( array_keys($array), array_keys( array_keys( $array )) ));
+    if( $associative ){
+
+        $construct = array();
+        foreach( $array as $key => $value ){
+
+            // We first copy each key/value pair into a staging array,
+            // formatting each key and value properly as we go.
+
+            // Format the key:
+            if( is_numeric($key) ){
+                $key = "key_$key";
+            }
+            $key = "'".addslashes($key)."'";
+
+            // Format the value:
+            if( is_array( $value )){
+                $value = array_to_json( $value );
+            } else if( !is_numeric( $value ) || is_string( $value ) ){
+                $value = "'".addslashes($value)."'";
+            }
+
+            // Add to staging array:
+            $construct[] = "$key: $value";
+        }
+
+        // Then we collapse the staging array into the JSON form:
+        $result = "{ " . implode( ", ", $construct ) . " }";
+
+    } else { // If the array is a vector (not associative):
+
+        $construct = array();
+        foreach( $array as $value ){
+
+            // Format the value:
+            if( is_array( $value )){
+                $value = array_to_json( $value );
+            } else if( !is_numeric( $value ) || is_string( $value ) ){
+                $value = "'".addslashes($value)."'";
+            }
+
+            // Add to staging array:
+            $construct[] = $value;
+        }
+
+        // Then we collapse the staging array into the JSON form:
+        $result = "[ " . implode( ", ", $construct ) . " ]";
+    }
+
+    return $result;
+}
+
+
+
+/**
 * Return status of job
 *
 */
@@ -18,14 +89,14 @@ function sapienta_api_jobstatus(){
 	$sc = new SAPIENTAClient();
 	$job =$sc->get_job_status($id);
 
-	print json_encode($job);
+	print array_to_json($job);
 }
 
 function sapienta_api_serverstats(){
 	$sc = new SAPIENTAClient();
 	$stats = $sc->get_service_status();
 	
-	print json_encode($stats);
+	print array_to_json($stats);
 }
 
 /**
@@ -65,17 +136,17 @@ function sapienta_api_submit(){
 				$sc = new SAPIENTAClient();
 				$job_id = $sc->submit_job($filename);
 
-				print json_encode(array("job_id" => $job_id));
+				print array_to_json(array("job_id" => $job_id));
 				
 
 			}else{ //couldn't move the file so there was a problem with the upload process
 				$message="There was a problem with the file upload.";
-				print json_encode(array("error" => $message));
+				print array_to_json(array("error" => $message));
 			}
 
 		}else{ //if the extension doesn't match pdf or xml
 			$message = "The filetype that you have uploaded is unsupported. Only PDF and XML files may be uploaded";
-			print json_encode(array("error" => $message));
+			print array_to_json(array("error" => $message));
 		}
 		
 	}
@@ -108,5 +179,8 @@ switch($_GET['action']){
 		print "Invalid operation.";
 	break;
 }
+
+
+
 
 ?>
