b9839fa0982464e7ee843434770d545d5fa9c154
James Ravenscroft <ravenscroftj@gmail.com>
2014-07-06 15:34:57 +0100

    Added empty junit test

---
+++ ssplit/test/uk/ac/aber/ssplit/TestSSPlit.java
@@ -0,0 +1,18 @@
+package uk.ac.aber.ssplit;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.Assert.*;
+
+public class TestSSPlit {
+
+    @Test
+    public void testSplit(){
+
+       
+    
+    
+    
+    }
+
+}
