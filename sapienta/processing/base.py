fb34a20a022988b711f2db02a50981e6f294f3a7
James Ravenscroft <ravenscroftj@gmail.com>
2014-12-01 21:03:48 +0000

    Did some cool stuff with mqtt and services and logging

--- sapienta/processing/base.py
+++ sapienta/processing/base.py
@@ -2,6 +2,7 @@
 Base MQTT client/service 
 """
 
+import json
 import logging
 import paho.mqtt.client as mqtt
 
@@ -11,11 +12,55 @@ from sapienta import app
 
 config = app.config
 
+class BaseService(object):
+    """This class provides a base MQTT service and an example
+    """
 
-class MQTTService:
+    def __init__(self):
+        self.logger = logging.getLogger(type(self).__name__)
+        self.logger.addHandler(MQTTLogHandler(logging.INFO, config))
+
+    def __call__(self,*args, **kwargs):
+        self.logger.info("Moo")
+        self.run(*args,**kwargs)
+
+    def run(self, config, message):
+        raise Exception("Unimplemented. Please extend this class")
+
+class HelloWorldService(BaseService):
+
+    def run(self, config, msg):
+        self.logger.info("message on topic {} says {}".format(msg.topic,msg.payload))
+
+class MQTTLogHandler(logging.Handler):
+
+    def __init__(self, level, config):
+        super(MQTTLogHandler,self).__init__(level)
+        self.config = config
+        self.client = mqtt.Client()
+
+        self.client.connect(self.config['SAPIENTA_MQTT_HOST'], 
+                self.config['SAPIENTA_MQTT_PORT'], 60)
+
+    def emit(self, record):
+
+        topic = "{}/{}/{}".format(self.config['SAPIENTA_LOGGING_PREFIX'],
+                record.levelname, record.name)
+
+        payload = json.dumps(record.__dict__)
+
+        self.client.publish(topic,payload)
+
+
+def run_service(servicetype, config, message):
+    service = servicetype()
+    service.run(config,message)
+
+class MQTTServiceBroker:
 
     def __init__(self, name, config):
         self.logger = logging.getLogger(name)
+        self.logger.addHandler(MQTTLogHandler(logging.INFO, config))
         self.services = {}
         self.config = config
         self.workers = Pool()
@@ -42,19 +87,31 @@ class MQTTService:
         #handle the message or discard it
         if message.topic in self.services:
             self.logger.info("Handing incoming message on {} to handler".format(message.topic))
-            
-            self.workers.apply_async(self.services[message.topic], 
-                    (client, userdata, message))
+
+            self.workers.apply_async(run_service,
+                    (self.services[message.topic],config,message))
+
+        elif message.topic.startswith(self.config['SAPIENTA_LOGGING_PREFIX']):
+            print message.payload
         elif message.topic == self.config['SAPIENTA_SHUTDOWN_TOPIC']:
             self.shutdown()
 
+    def on_work_done(self, result):
+        self.logger.info(result)
+
     def on_connect(self, client, userdata, flags, rc):
+        #subscribe for logging
+        client.subscribe(self.config['SAPIENTA_LOGGING_PREFIX'] + "#")
+        
         #subscribe to topics
         for subscription in self.services:
             client.subscribe(subscription)   
             self.logger.info("Subscribed to topic '{}'".format(subscription))
 
         #subscribe for shutdown topic
+        self.logger.info("Subscribed to topic {} for shutdown requests"
+                .format(self.config['SAPIENTA_SHUTDOWN_TOPIC']))
+
         client.subscribe(self.config['SAPIENTA_SHUTDOWN_TOPIC'])
 
     def shutdown(self):
@@ -68,5 +125,6 @@ class MQTTService:
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    service = MQTTService("SapientaService", config)
-    service.start()
+    service_broker = MQTTServiceBroker("SapientaService", config)
+    service_broker.services['sapienta/service/hello'] = HelloWorldService
+    service_broker.start()
