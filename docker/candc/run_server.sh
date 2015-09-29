4124c35ecd1750d72e65aeb2ea1fb5b17f30238d
James Ravenscroft <ravenscroftj@gmail.com>
2015-09-29 10:00:36 +0100

    Fixed run_server.sh to bind to all interfaces inside docker

--- docker/candc/run_server.sh
+++ docker/candc/run_server.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./soap_server --models ../../models --candc-pos ../../models/pos_bio-1.00 --candc-pos-maxwords 900 --candc-parser ../../models/parser --candc-super ../../models/super_coresc/ --candc-parser-markedup ../../models/parser/cats/markedup_new --candc-super-maxwords 900 --candc-parser-maxwords 900 --server 127.0.0.1:9004
+./soap_server --models ../../models --candc-pos ../../models/pos_bio-1.00 --candc-pos-maxwords 900 --candc-parser ../../models/parser --candc-super ../../models/super_coresc/ --candc-parser-markedup ../../models/parser/cats/markedup_new --candc-super-maxwords 900 --candc-parser-maxwords 900 --server 0.0.0.0:9004
