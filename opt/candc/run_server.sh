31257db621c06974a35eca45767b6f41d92e5725
James Ravenscroft <jamesrav@uk.ibm.com>
2015-12-18 09:03:06 +0000

    Modified run server script for candc to make it run on any port and to restart when it falls over

--- opt/candc/run_server.sh
+++ opt/candc/run_server.sh
@@ -1,2 +1,18 @@
 #!/bin/sh
-./soap_server --models ../../models --candc-pos ../../models/pos_bio-1.00 --candc-pos-maxwords 900 --candc-parser ../../models/parser --candc-super ../../models/super_coresc/ --candc-parser-markedup ../../models/parser/cats/markedup_new --candc-super-maxwords 900 --candc-parser-maxwords 900 --server 127.0.0.1:9004
+
+
+if [ $# -lt 1 ]
+then
+    PORT=9004
+else
+    PORT=$1
+fi
+
+echo $PORT
+
+ARGS="--models ../../models --candc-pos ../../models/pos_bio-1.00 --candc-pos-maxwords 900 --candc-parser ../../models/parser --candc-super ../../models/super_coresc/ --candc-parser-markedup ../../models/parser/cats/markedup_new --candc-super-maxwords 900 --candc-parser-maxwords 900 --server 127.0.0.1:$PORT"
+
+until ./soap_server $ARGS; do
+    echo "soap server crashed. Respawning..." >&2
+    sleep 1
+done
