d91e550f1858c0ca17a34f1f574f55b7a04fdf99
James Ravenscroft <ravenscroftj@gmail.com>
2021-01-18 19:54:41 +0000

    ignore test as part of find_packages in setup script

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name = "SAPIENTA",
     version = 0.1,
-    packages = find_packages(),
+    packages = find_packages(exclude=('*.test')),
 
     #install requirements
     install_requires = [],
